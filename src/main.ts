9eb316f10b6cc28de0a653f41e877cc912e09c13
bakabaka9405 <2677423642@qq.com>
2025-03-10 13:31:10 +0800

    feat: add naive-ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import naive from "naive-ui";
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
 
-createApp(App).mount("#app");
+createApp(App).use(naive).mount("#app");
